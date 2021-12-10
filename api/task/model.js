06affc826456691d9360035df7c5a105d52870d0
Arison13 <arisonarias@gmail.com>
2021-12-10 13:42:59 -0500

    all test passing can GET and POST new tasks!

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,53 @@
 // build your `Task` model here
+const db = require('../../data/dbConfig')
+
+module.exports = {
+    getTasks,
+    getTaskById,
+    createTask
+}
+
+async function getTasks(){
+    const rawData = await db('tasks')
+    .leftJoin('projects', 'projects.project_id','tasks.project_id')
+    .select('tasks.*', 'projects.project_name','projects.project_description')
+    
+    const clientData = rawData.map(task => {
+        if(!task.task_completed){
+            return {
+                ...task,
+                task_completed: false
+            }
+        }else {
+            return {
+                ...task,
+                task_completed: true,
+            }
+        }
+    })
+    return clientData 
+} 
+
+async function getTaskById(task_id){
+    const rawData = await db('tasks')
+    .leftJoin('projects', 'projects.project_id','tasks.project_id')
+    .select('tasks.*', 'projects.project_name','projects.project_description')
+    .where('task_id', task_id)
+    .first()
+
+     const clientData = !rawData
+        ? { ...rawData, task_completed: false}
+        : { ...rawData, task_completed: true }
+    return clientData 
+} 
+
+async function createTask(newTask){
+    const newId = await db('tasks').insert(newTask);
+    const createdTask = await db('tasks').select('*').where('task_id', newId).first()
+
+    const clientData = !createdTask.task_completed
+        ? { ...createdTask, task_completed: false }
+        : { ...createdTask, task_completed: true}
+
+    return clientData;
+}
--- api/task/router.js
+++ api/task/router.js
@@ -1,10 +1,30 @@
 // build your `/api/tasks` router here
 const express = require('express');
-
+const Task = require('./model')
 const router = express.Router();
 
-router.get('/', (req,res) => {
-    res.send("HELLO FROM ROUTER")
+router.get('/', (req,res, next) => {
+    Task.getTasks()
+    .then (task => {
+        res.json(task)
+    })
+    .catch(next)
 })
+router.get('/:task_id', (req,res, next) => {
+
+    Task.getTaskById(req.params.task_id)
+    .then (task => {
+        res.json(task)
+    })
+    .catch(next)
+})
+
+router.post('/', (req, res, next) => {
+    Task.createTask(req.body)
+        .then(newTask => {
+            res.status(201).json(newTask)
+        })
+        .catch(next)
+});
 
 module.exports = router;
