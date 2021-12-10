25ed03f9e49a33ada7431b719f43a0440e955fc3
Arison13 <arisonarias@gmail.com>
2021-12-10 13:24:53 -0500

    can GET and POST projects

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,58 @@
 // build your `Project` model here
+const { raw } = require('express')
+const db = require('../../data/dbConfig')
+
+module.exports = {
+    getProjects,
+    getProjectById,
+    createProject
+}
+
+async function getProjects() {
+    const rawData = await db('projects').select('*') 
+    
+    const clientData = rawData.map(project => {
+        if(!project.project_completed){
+            return {
+                ...project,
+                project_completed: false
+            }
+        }else {
+            return {
+                ...project,
+                project_completed: true,
+            }
+        }
+    })
+    return clientData
+}
+
+async function getProjectById(project_id) {
+    const rawData  = await db('projects')
+        .select("*")
+        .where('project_id', project_id)
+        .first()
+        if(!rawData.project_completed) {
+            return {
+                ...rawData,
+                project_completed:false
+            }
+        }else {
+            return{
+                ...rawData,
+                project_completed: true
+            }
+        }
+}
+
+async function createProject(newProject) {
+    const newId = await db("projects").insert(newProject);
+  
+    const getNewProject = await getProjectById(newId);
+  
+    const addBool = !getNewProject.project_completed
+      ? { ...getNewProject, project_completed: false }
+      : { ...getNewProject, project_completed: true };
+  
+    return addBool;
+  }
--- api/project/router.js
+++ api/project/router.js
@@ -1,10 +1,33 @@
 // build your `/api/projects` router here
 const express = require('express');
-
+const Projects = require('./model')
 const router = express.Router();
 
-router.get('/', (req,res) => {
-    res.send("HELLO FROM ROUTER")
+router.get('/', async (req,res, next) => {
+    const data = await Projects.getProjects()
+    try {
+        res.json(data)
+    }
+    catch (err) {
+        next(err)
+    }
 })
+
+router.get('/:project_id', (req,res, next) => {
+    Projects.getProjectById(req.params.project_id)
+    .then ( project => {
+        res.json(project)
+    })
+    .catch(next) 
+})
+
+router.post("/", (req, res, next) => {
+    Projects.createProject(req.body)
+      .then(newProject => {
+        res.status(201).json(newProject);
+      })
+      .catch(next);
+  });
+
 
 module.exports = router;
